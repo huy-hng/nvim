8350da9b3bd785a6d4f407df567d900ad9923437
Huy <huy@hoang.dev>
2022-12-05 17:45:42 +0100

    refactor init.lua to necessary and extra features

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -4,24 +4,34 @@ NVIM_CONFIG_PATH = os.getenv('HOME') .. '/.config/nvim/'
 package.path = NVIM_CONFIG_PATH .. '?.lua;' .. package.path
 
 local require_dir = require('core.require_dir')
-require_dir('lua/core')
 
-require('options')
-require('colorscheme')
-require('plugins')
-require('autocmd')
-require('lazyloader')
 
-if vim.g.neovide then require('neovide') end
+local function minimal_init()
+	require_dir('lua/core')
 
-require_dir('lua/functions') -- should come before keymaps
-require_dir('lua/keymaps')
+	require('options') -- save
+	require('colorscheme') -- catpuccin needs to be installed
+	require('autocmd') --
+	-- require('lazyloader') -- needs a crap ton of plugins
 
-require('plugins.lualine')
--- require('plugins.bufferline')
--- require('plugins.quickscope')
--- require('plugins.startify')
--- require('plugins.vimwiki')
--- require('plugins.zen')
+	require_dir('lua/functions') -- should come before keymaps
+	require_dir('lua/keymaps')
+end
 
--- pcall(require, 'plugins.tree')
+local function extra_features()
+	Prequire('plugins')
+	Prequire('lazy_plugins')
+
+	Prequire('plugins.lualine')
+	Prequire('plugins.alpha')
+	Prequire('plugins.packer_compiled')
+	require_dir('lua/plugins/early_load_plugins')
+
+	if vim.g.neovide then require('neovide') end
+end
+
+minimal_init()
+
+if not vim.g.minimal then
+	extra_features()
+end
