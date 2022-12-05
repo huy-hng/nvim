ca2a2ee9add83b5304ef8bc90d451bad3ec12684
Huy <huy@hoang.dev>
2022-12-05 17:59:05 +0100

    refactoring and trying to fix cmp lsp order

--- vim/.config/nvim/lua/plugins/cmp/init.lua
+++ vim/.config/nvim/lua/plugins/cmp/init.lua
@@ -6,6 +6,58 @@ if not has_luasnip then return end
 
 local kind_icons = require('plugins.cmp.icons')
 
+local CompletionItemKind = {
+	Text = 1,
+	Method = 2,
+	Function = 3,
+	Constructor = 4,
+	Field = 5,
+	Variable = 6,
+	Class = 7,
+	Interface = 8,
+	Module = 9,
+	Property = 10,
+	Unit = 11,
+	Value = 12,
+	Enum = 13,
+	Keyword = 14,
+	Snippet = 15,
+	Color = 16,
+	File = 17,
+	Reference = 18,
+	Folder = 19,
+	EnumMember = 20,
+	Constant = 21,
+	Struct = 22,
+	Event = 23,
+	Operator = 24,
+	TypeParameter = 25,
+}
+local reverse = vim.tbl_add_reverse_lookup(CompletionItemKind)
+
+local kind_mapper =
+	{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 }
+
+-- %s/^<\(.*\)>{/local entry_\1 = {
+-- %s/= <\(.*\)>{/= {
+-- %s/<\(.*\)> = {/\1 = {
+-- %s/= <\(.*\)>{/=
+-- %s/ <\(.*\)>{/{/
+-- %s/= <\(.*\)>,/= '\1',/
+-- %s/= <\(.*\)>/= '\1'/
+-- %s/<\(table .\)>,/'\1',/g
+-- %s/<\(table ..\)>,/'\1',/g
+-- %s/, <\(table .\)>//g
+
+local function redir(data)
+	return function()
+		vim.cmd.redir('>> get_kind.lua')
+		vim.pretty_print(data)
+		vim.cmd.redir('END')
+	end
+end
+
+Counter = 0
 cmp.setup {
 	snippet = {
 		expand = function(args)
@@ -31,12 +83,12 @@ cmp.setup {
 		-- fields = { 'abbr', 'menu' },
 		format = function(entry, vim_item)
 			-- Kind icons
-			vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			-- vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
+			vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
+				luasnip = '[Snippet]',
 				nvim_lsp = '[LSP]',
 				-- nvim_lua = '[API]',
-				luasnip = '[Snippet]',
 				buffer = '[Buffer]',
 				cmdline_history = '[History]',
 				path = '[Path]',
@@ -69,6 +121,26 @@ cmp.setup {
 		},
 		{ name = 'path', keyword_length = 3 },
 	}),
+	sorting = {
+		comparators = {
+			function(entry1, entry2)
+				Counter = Counter + 1
+
+				-- if Counter % 10 == 0 then
+				-- local kind = reverse[entry1:get_kind()]
+				-- vim.schedule(redir(kind))
+				-- if Counter == 1 then
+				-- 	vim.schedule(redir(entry1))
+				-- end
+				local kind1 = kind_mapper[entry1:get_kind()]
+				local kind2 = kind_mapper[entry2:get_kind()]
+
+				-- P(entry1)
+				-- P(entry2)
+				if kind1 < kind2 then return true end
+			end,
+		},
+	},
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
