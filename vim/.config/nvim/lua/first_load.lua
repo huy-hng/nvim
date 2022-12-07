47cae7764028fad8988f2f49b88a0985198e9cf9
Huy <huy@hoang.dev>
2022-12-07 20:43:11 +0100

    fix first load script

--- vim/.config/nvim/lua/first_load.lua
+++ vim/.config/nvim/lua/first_load.lua
@@ -1,5 +1,5 @@
 local download_packer = function()
-	if vim.fn.input { 'Download Packer? (y for yes)' } ~= 'y' then return end
+	if vim.fn.input ( 'Download Packer? (y for yes)' ) ~= 'y' then return end
 
 	local directory = string.format('%s/site/pack/packer/start/', vim.fn.stdpath('data'))
 
@@ -9,14 +9,14 @@ local download_packer = function()
 		string.format(
 			'git clone %s %s',
 			'https://github.com/wbthomason/packer.nvim',
-			directory .. '/packer.nvim'
+			directory .. 'packer.nvim'
 		)
 	)
 
 	print(out)
 	print('Downloading packer.nvim...')
 	print("( You'll need to restart now )")
-	vim.cmd([[qa]])
+	-- vim.cmd([[qa]])
 end
 
 return function()
