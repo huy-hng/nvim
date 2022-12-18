843ed6a3f8f37e730a17702dc6783c534e59da29
Huy <huy@hoang.dev>
2022-12-18 22:28:02 +0100

    added snippet for creating dividers (sections)

--- vim/.config/nvim/lua/plugins/snippets/ft/all.lua
+++ vim/.config/nvim/lua/plugins/snippets/ft/all.lua
@@ -1,19 +1,5 @@
 ---@diagnostic disable: undefined-global
 
-local ls = require('luasnip')
-
-local s = ls.s
-local snippet_from_nodes = ls.sn
-
-local i = ls.insert_node
-local t = ls.text_node
-local d = ls.dynamic_node
-local c = ls.choice_node
-local f = ls.function_node
-
-local events = require('luasnip.util.events')
-
-
 local shortcut = function(val)
 	if type(val) == 'string' then return { t { val }, i(0) } end
 
@@ -25,11 +11,11 @@ local shortcut = function(val)
 
 	return val
 end
+
 local same = function(index)
-	return f(function(args)
-		return args[1]
-	end, { index })
+	return f(function(args) return args[1] end, { index })
 end
+
 local make = function(tbl)
 	local result = {}
 	for k, v in pairs(tbl) do
@@ -39,9 +25,134 @@ local make = function(tbl)
 	return result
 end
 
+local function comment() return vim.bo.commentstring:gsub(' %%s', '') end
 
-local toexpand_count = 0
+local function divider_creator(text_len, divider_len, char)
+	local indent_level = vim.fn.indent(vim.fn.line('.'))
+	local length = (divider_len - text_len - indent_level - #comment()) / 2
+	return Repeat(char, math.floor(length))
+end
+
+local function divider_nodes(text_len, char_vars, len_vars)
+	local variations = {}
+	for _, divider_len in ipairs(len_vars) do
+		for _, char in ipairs(char_vars) do
+			table.insert(variations, t(divider_creator(text_len, divider_len, char)))
+		end
+	end
+	return variations
+end
+
+local function divider_(args, _)
+	local text = args[1][1] or ''
+	local char_variations = { '-', '=' }
+	local length_variations = { 100, 50 }
+
+	local text_len = vim.fn.strdisplaywidth(text)
+	local sn = snippet_from_nodes(nil, {
+		c(1, divider_nodes(text_len, char_variations, length_variations)),
+	})
+	sn.len = 1
+	return sn
+end
+
+local function divider(side, char, divider_len)
+	return function(args, _, old_state)
+		local text = args[1][1] or ''
+
+		local text_len = vim.fn.strdisplaywidth(text)
+
+		local indent_level = vim.fn.indent(vim.fn.line('.'))
+		local length = (divider_len - text_len - indent_level - #comment()) / 2
+		local round_fn = side == 'right' and math.ceil or math.floor
+
+		return snippet_from_nodes(nil, {
+			t(Repeat(char, round_fn(length))),
+		})
+	end
+end
+
+local function choices(char_vars, len_vars)
+	local variations = {}
+	for _, divider_len in ipairs(len_vars) do
+		for _, char in ipairs(char_vars) do
+			table.insert(
+				variations,
+				sn(nil, {
+					d(2, divider('left', char, divider_len), { 1 }),
+					r(1, 'user_text'),
+					d(3, divider('right', char, divider_len), { 1 }),
+				})
+			)
+		end
+	end
+	return variations
+end
+
 return {
+
+	s('rest', {
+		i(1, 'preset'),
+		t { '', '' },
+		d(2, function(args, _) --
+			return sn(nil, {
+				i(1, args[1]),
+				r(2, 'dyn', i(nil, 'user_text')),
+			})
+		end, 1),
+	}),
+
+	s('trig2', {
+		i(1, 'change to update'),
+		d(2, function(args, _, old_state)
+			vim.notify(vim.inspect(args[1]))
+			vim.notify(vim.inspect(old_state))
+			old_state = old_state or {
+				updates = 0,
+			}
+
+			old_state.updates = old_state.updates + 1
+
+			local snip = sn(nil, {
+				t(tostring(old_state.updates)),
+			})
+
+			snip.old_state = old_state
+			return snip
+		end, { 1 }),
+	}),
+
+	snippet('div', {
+		f(comment),
+		c(1, choices({ '-', '=' }, { 100, 50 })),
+	}),
+	snippet(
+		'one',
+		fmt(
+			[[{comment}{divider}{text}{divider}
+
+			]],
+			{
+				comment = f(comment),
+				divider = d(2, divider, { 1 }),
+				text = i(1),
+			},
+			{ repeat_duplicates = true }
+		)
+	),
+	s('paren_change', {
+		c(1, {
+			sn(nil, { t('('), r(1, 'user_text'), t(')') }),
+			sn(nil, { t('['), r(1, 'user_text'), t(']') }),
+			sn(nil, { t('{'), r(1, 'user_text'), t('}') }),
+		}),
+	}, {
+		stored = {
+			-- key passed to restoreNodes.
+			['user_text'] = i(1, 'default_text'),
+		},
+	}),
+
 	-- s('cond', {
 	-- 	t('will only expand in c-style comments'),
 	-- 	}, {
@@ -60,16 +171,7 @@ return {
 	-- 	i(3, 'else'),
 	-- }),
 	-- -- callbacks table
-	-- s('toexpand', c(1, { t('hello'), t('world'), t('last') }), {
-	-- 	callbacks = {
-	-- 		[1] = {
-	-- 			[events.enter] = function(--[[ node ]])
-	-- 				toexpand_count = toexpand_count + 1
-	-- 				print('Number of times entered:', toexpand_count)
-	-- 			end,
-	-- 		},
-	-- 	},
-	-- }),
+	-- s('toexpand', c(1, { t('hello'), t('world'), t('last') })),
 
 	-- -- regTrig
 	-- --    snippet.captures
@@ -129,7 +231,7 @@ return {
 	-- type the first few words of a commit message -> expands to commit hash that matches
 	-- commit:Fixes #
 
-	-- tree sitter
+	-- treesitter
 	-- :func:x -> find all functions in the file with x in the name, and choice between them
 
 	-- auto-insert markdown footer?
