b7d11d2647f99bdae97f94fc978c8cd69b6fa744
Huy <huy@hoang.dev>
2022-12-21 13:49:58 +0100

    move keymaps to after

--- vim/.config/nvim/lua/keymaps/function_keymaps.lua
+++
@@ -1,3 +0,0 @@
-nmap('%', MatchCharacter, 'Jump to matching quote or whatever')
--- nmap('<leader>m', '<cmd>message<cr>', 'Show Messages')
-nmap('<leader>m', '<cmd>40message<cr>', 'Show Messages')
--- vim/.config/nvim/lua/keymaps/plugin_maps.lua
+++
@@ -1,3 +0,0 @@
-nmap('<leader>z', CMD('ZenMode'), 'Zen Mode')
-nmap('<leader>Z', CMD('Twilight'), 'Twilight')
-nmap('<F5>', CMD('UndotreeToggle'), 'Undo Tree')
