dbce3eb6e0a3193f0e140ac776c60a974dc1d279
Huy <huy@hoang.dev>
2022-12-24 22:39:40 +0100

    fix columnline bugs

--- vim/.config/nvim/lua/functions/column_line.lua
+++ vim/.config/nvim/lua/functions/column_line.lua
@@ -1,29 +1,32 @@
+-- used in autoccommands
 ----------------------------------------------Config------------------------------------------------
+local M = {}
 
-vim.go.colorcolumn = ''
-vim.opt_global.colorcolumn:append { 101 }
-vim.api.nvim_set_hl(0, 'ColorColumn', {
-	fg = '#45475a',
-	-- sp = 'red',
-	-- link = 'Whitespace',
-	-- bg = 'black',
-	-- blend = 100,
-})
+vim.opt_global.colorcolumn:append { 100 }
 
 local column_char = '▏'
 local namespace = vim.api.nvim_create_namespace('ColumnLine')
 
+-- local no_column_line = vim.api.nvim_create_namespace('no_column_line')
+Highlight(namespace, 'ColumnLine', {})
+
+Highlight(0, 'ColumnLine', {
+	fg = '#45475a',
+	-- link = 'ColorColumn'
+})
+
+Highlight(0, 'ColorColumn', {
+	fg = '#45475a',
+	-- link = 'ColorColumn'
+})
+
 ---------------------------------------------Functions----------------------------------------------
 
 local function should_set(line_text, column)
-	-- P(line_text)
+	column = column + 1
 	local width = vim.fn.strdisplaywidth(line_text)
 	if width < column then return true end
-	-- local char_index = vim.fn.strgetchar(line_text, column) + 2
-	-- print(line_text)
-	-- print(width, char_index, string.sub(line_text, char_index, char_index))
-
-	-- vim.fn.strcharpart()
+	local test = print(' ', 'asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd')
 
 	line_text = line_text:gsub('\t', string.rep(' ', vim.opt.tabstop:get()))
 	local char = string.sub(line_text, column, column)
@@ -37,16 +40,17 @@ local function set_line(bufnr, column)
 		if not should_set(line_text, column) then
 			goto continue
 		end
+
 		vim.api.nvim_buf_set_extmark(bufnr, namespace, linenr - 1, 0, {
-			virt_text = { { column_char, 'ColorColumn' } },
+			virt_text = { { column_char, 'ColumnLine' } },
 			virt_text_pos = 'overlay',
 			hl_mode = 'combine',
-			virt_text_win_col = column - 1,
+			virt_text_win_col = column,
 			priority = 1,
 			-- hl_group = 'NormalFloat',
 			-- end_col = 100,
 			-- hl_eol = true,
-			-- virt_lines = { { { column_char, 'ColorColumn' } } },
+			-- virt_lines = { { { column_char, 'ColumnLine' } } },
 			-- line_hl_group = 'Search',
 			-- cursorline_hl_group = 'Visual',
 			-- conceal = 'a',
@@ -57,16 +61,35 @@ local function set_line(bufnr, column)
 	end
 end
 
-local function refresh()
+function M.refresh()
 	-- if force then
 	-- 	local win = vim.api.nvim_get_current_win()
 	-- 	-- vim.cmd([[noautocmd windo lua refrfeshcolumnline()]])
 	-- 	if vim.api.nvim_win_is_valid(win) then vim.api.nvim_set_current_win(win) end
 	-- end
 
-	local bufnr = vim.api.nvim_get_current_buf()
+	local ignore_wintypes = {
+		'autocmd',
+		'command',
+		'loclist',
+		-- 'popup',
+		-- 'preview',
+		'quickfix',
+		'unknown',
+	}
 
+	local bufnr = vim.api.nvim_get_current_buf()
 	if not vim.api.nvim_buf_is_loaded(bufnr) then return end
+
+	local modifiable = vim.api.nvim_buf_get_option(bufnr, 'modifiable')
+
+	local type = Util.win_type()
+	if not modifiable or vim.tbl_contains(ignore_wintypes, type) then
+		vim.api.nvim_win_set_hl_ns(0, namespace)
+		return
+	end
+	vim.api.nvim_win_set_hl_ns(0, 0)
+
 	if namespace then vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1) end
 
 	for _, column in ipairs(vim.opt.colorcolumn:get()) do
@@ -74,26 +97,4 @@ local function refresh()
 	end
 end
 
------------------------------------------------Setup------------------------------------------------
-
-Augroup('ColumnLine', {
-	Autocmd('OptionSet', 'colorcolumn', refresh),
-
-	Autocmd({
-		'FileChangedShellPost',
-		'TextChanged',
-		'TextChangedI',
-		'CompleteChanged',
-		'VimEnter',
-		'SessionLoadPost',
-		-- 'BufEnter',
-		-- 'BufWinEnter',
-		-- 'WinEnter',
-	}, function()
-		local mode = vim.fn.mode()
-		if mode == 'c' then return end
-		refresh()
-	end),
-}, true, true)
-
-return refresh
+return M
