68241e2f174abdd386ab19d572f860d91e9960aa
Huy <huy@hoang.dev>
2022-12-31 13:30:11 +0100

    use plugin file for columnline

---
+++ lua/plugins/column_line.lua
@@ -0,0 +1,25 @@
+local M = {
+	'huy-hng/column_line.nvim',
+	dev = true,
+}
+
+function M.config()
+	local column_line = R('column_line')
+	column_line.setup()
+	-- TODO: move this to the plugin folder, needs augroup and autocmd functions
+	Augroup('ColumnLine', {
+		Autocmd('OptionSet', 'colorcolumn', column_line.refresh),
+		Autocmd({
+			'FileChangedShellPost',
+			'TextChanged',
+			'TextChangedI',
+			'CompleteChanged',
+			'VimEnter',
+			'SessionLoadPost',
+			'BufWinEnter',
+			'WinEnter',
+		}, column_line.refresh),
+	})
+end
+
+return M
