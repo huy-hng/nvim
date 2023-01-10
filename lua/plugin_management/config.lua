1aee466dabddafe7c2785f19bcdb1589c74e7a4c
Huy <huy@hoang.dev>
2023-01-10 18:56:54 +0100

    .

--- lua/plugin_management/config.lua
+++ lua/plugin_management/config.lua
@@ -18,7 +18,7 @@ return {
 	},
 	dev = {
 		-- directory where you store your local plugin projects
-		path = '~/.dotfiles/nvim/.config/nvim/plugins/',
+		path = '~/.dotfiles/nvim/.config/nvim/my_plugins/',
 		---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
 		patterns = {}, -- For example {"folke"}
 	},
