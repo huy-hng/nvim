ecf580c57ff4af3ddd89c2f1cff3aa95c573862d
Huy <huy@hoang.dev>
2023-01-10 15:04:11 +0100

    change require strategy

--- lua/plugin_management/init.lua
+++ lua/plugin_management/init.lua
@@ -1,7 +1,16 @@
 require('plugin_management.ensure_lazy')
 
 local config = require('plugin_management.config')
-require('lazy').setup('plugins', config)
+-- require('lazy').setup('plugins', config)
+require('lazy').setup({
+	{ import = 'plugins' },
+	{ import = 'plugins.coding' },
+	{ import = 'plugins.editor' },
+	{ import = 'plugins.git' },
+	{ import = 'plugins.lsp' },
+	-- { import = 'plugins.neorg' },
+	{ import = 'plugins.ui' },
+}, config)
 
 -- local group = vim.api.nvim_create_augroup('startertest2',{})
 -- vim.api.nvim_create_autocmd('User', {
--- lua/plugins/git/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-	require('plugins.git.gitsigns'),
-	require('plugins.git.neogit'),
-	require('plugins.git.diffview'),
-}
