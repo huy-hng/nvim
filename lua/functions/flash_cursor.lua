3da65b2debb0b9995d0a739ab2409f9c9ebfe8bf
Huy <huy@hoang.dev>
2023-01-27 00:46:46 +0100

    restore previous settings

--- lua/functions/flash_cursor.lua
+++ lua/functions/flash_cursor.lua
@@ -1,10 +1,14 @@
+local function set_options(opts)
+	vim.o.cursorlineopt = opts.cursorlineopt
+	vim.o.cursorcolumn = opts.cursorcolumn
+	vim.o.cursorline = opts.cursorline
+end
 local function show_cursor(show)
-	vim.cmd('hi CursorColumn guibg=#3a3b4c')
-	vim.cmd('hi CursorLine guibg=#3a3b4c')
-	-- vim.cmd('hi CursorLine guibg=#2a2b3c')
-	vim.o.cursorlineopt = 'screenline,number'
-	vim.o.cursorline = show
-	vim.o.cursorcolumn = show
+	set_options {
+		cursorlineopt = 'screenline,number',
+		cursorcolumn = show,
+		cursorline = show,
+	}
 	vim.cmd.redraw()
 end
 
@@ -14,10 +18,21 @@ local function blink(duration)
 end
 
 return function(times, blink_time)
+	vim.cmd('hi CursorColumn guibg=#3a3b4c')
+	vim.cmd('hi CursorLine guibg=#3a3b4c')
+
+	local opts_before = {
+		cursorlineopt = vim.o.cursorlineopt,
+		cursorcolumn = vim.o.cursorcolumn,
+		cursorline = vim.o.cursorline,
+	}
+
 	times = times == nil and 1 or times
 	blink_time = blink_time == nil and 200 or blink_time
 	for i = 1, times do
 		local wait = blink_time * 2 * (i - 1)
 		nvim.defer(wait, blink, blink_time)
 	end
+
+	nvim.defer(2 * blink_time * (times + 1), set_options, opts_before)
 end
