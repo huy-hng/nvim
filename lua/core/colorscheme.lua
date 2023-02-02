860eb207b54c56e7075e5998012a4fcd6ce4f423
Huy <huy@hoang.dev>
2023-02-02 16:16:12 +0100

    add default colors

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -14,6 +14,21 @@ function SetColors(theme)
 
 	if ColorTheme == 'default' then return end
 
+	-- Highlight(0, 'Folded', { bg = '#45475A' })
+	-- Highlight(0, 'Folded', { bg = '#1E1E2E' })
+	Highlight(0, 'Folded', { bg = '#1F1F2F' })
+	-- Highlight(0, 'Folded', { bg = nil })
+
+	-- vim.cmd('highlight Statusline guibg=none')
+	-- Highlight(0, 'IndentLine', { fg = '#45475a' })
+	-- Highlight(0, 'ColumnLine', { fg = '#45475a' })
+	Highlight(0, 'IndentLine', { link = 'Comment' })
+	Highlight(0, 'ColumnLine', { link = 'Comment' })
+
+	-- vim.cmd('highlight ColorColumn guibg=none guifg=#45475a')
+
+	if ColorTheme == 'default' then return end
+
 	-- Highlight(0, 'Statusline', { fg = 'fg' })
 	-- Highlight(0, 'CursorColumn', { link = 'CursorLine' })
 	-- Highlight(0, 'helpCommand', { link = 'markdownCode' })
@@ -24,4 +39,4 @@ end
 -- 'tokyonight-storm'
 -- 'tokyonight-moon'
 -- 'tokyonight-night'
--- SetColors()
+-- SetColors('catppuccin')
