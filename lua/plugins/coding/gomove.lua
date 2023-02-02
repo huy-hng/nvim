b7a04b8a70fb5639d9b017e8fd6f467d4360866e
Huy <huy@hoang.dev>
2023-02-02 16:16:42 +0100

    unmap default maps for gomove

--- lua/plugins/coding/gomove.lua
+++ lua/plugins/coding/gomove.lua
@@ -5,7 +5,7 @@ local M = {
 
 function M.config()
 	require('gomove').setup {
-		map_defaults = true, -- whether or not to map default key bindings, (true/false)
+		map_defaults = false, -- whether or not to map default key bindings, (true/false)
 		reindent = true,
 		undojoin = true, -- whether or not to undojoin same direction moves
 		move_past_end_col = true, -- whether to not to move past end column when moving blocks horizontally
