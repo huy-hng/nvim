0615736c44243ef77b94cf68d09b07ad20d3db31
Huy <huy@hoang.dev>
2023-02-10 00:47:00 +0100

    improve animation api

--- init.lua
+++ init.lua
@@ -1,8 +1,7 @@
----------------------------------------------Set Path-----------------------------------------------
-
-NVIM_CONFIG_PATH = os.getenv('HOME') .. '/.config/nvim/'
+NVIM_CONFIG_PATH = vim.fn.stdpath('config')
 package.path = NVIM_CONFIG_PATH .. '?.lua;' .. package.path
 
+vim.g.neovide_fullscreen = true
 vim.g.has_neovide = vim.g.neovide
 vim.g.neovide = nil
 
@@ -10,16 +9,23 @@ require('core.options')
 require('core.globals')
 require('core.types')
 require('core.colorscheme')
+require('core.statuscolumn')
 
-require('plugin_management')
+require('plugin_management.lazy')
 require('core.neovide')
 
-if vim.fn.has('nvim-0.9.0') == 1 then require('core.statuscolumn') end
+-- local files = vim.api.nvim_get_runtime_file(vim.fn.stdpath('config'), true)
+-- local file_paths = vim.api.nvim_get_runtime_file(dir .. '/**/*.lua', true)
+-- local files = vim.api.nvim_get_runtime_file('lua/**/*.lua', true)
+-- print('============')
+-- for _, item in ipairs(files) do
+-- 	local relative = string.match(item, 'nvim/lua/(.*).lua$')
+-- 	if relative then print(item) end
+-- end
 
 Augroup('LazyLoad', {
 	Autocmd('User', 'VeryLazy', function()
 		local require_dir = require('modules.require_dir')
-
 		require('core.autocmd')
 		require_dir('lua/functions')
 		require_dir('lua/keymaps')
