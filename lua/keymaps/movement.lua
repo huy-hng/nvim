fa4166d9d838f4cd9272d60481159f6f64f46968
Huy <huy@hoang.dev>
2023-02-15 21:05:47 +0100

    small changes

--- lua/keymaps/movement.lua
+++ lua/keymaps/movement.lua
@@ -31,23 +31,27 @@ ICmap('<A-l>', { nvim.feedkeys, '<Right>' })
 -- cmap('<C-k>', BindFeedkeys('<Up>'))
 Cmap('<C-n>', { nvim.feedkeys, '<Down>' })
 Cmap('<C-p>', { nvim.feedkeys, '<Up>' })
+local function move_to_indent()
+	local has_indent, ts_indent = pcall(require, 'nvim-treesitter.indent')
+	if not has_indent then return end
+	if vim.fn.col('$') > 1 then return end
+
+	local line_nr = vim.fn.line('.')
+	if vim.fn.indent(line_nr) > 0 then return end
+
+	-- returns -1 when the buffer has no parser
+	local total_indent = ts_indent.get_indent(line_nr)
+	if total_indent <= 0 then return end
+
+	local indent_width = vim.bo.expandtab and vim.bo.shiftwidth or vim.bo.tabstop
+
+	local tabs = Repeat('<tab>', total_indent / indent_width)
+	return string.format('A%s', tabs)
+end
 
 Nmap('A', 'A', 'Move to correct indentation, or normal A', {
-	callback = function()
-		local has_indent, ts_indent = pcall(require, 'nvim-treesitter.indent')
-		if not has_indent then return end
-		if vim.fn.col('$') > 1 then return end
-
-		local line_nr = vim.fn.line('.')
-		if vim.fn.indent(line_nr) > 0 then return end
-
-		-- returns -1 when the buffer has no parser
-		local total_indent = ts_indent.get_indent(line_nr)
-		if total_indent <= 0 then return end
-
-		local indent_width = vim.bo.expandtab and vim.bo.shiftwidth or vim.bo.tabstop
-
-		local tabs = Repeat('<tab>', total_indent / indent_width)
-		return string.format('A%s', tabs)
-	end,
+	callback = move_to_indent,
+})
+Nmap('a', 'a', 'Move to correct indentation, or normal A', {
+	callback = move_to_indent,
 })
