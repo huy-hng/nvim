75be4b0c86cb33ae7964f1c97b312a695f8b0c9a
Huy <huy@hoang.dev>
2023-02-15 21:05:21 +0100

    disable cmp documentation window

--- lua/plugins/coding/cmp/init.lua
+++ lua/plugins/coding/cmp/init.lua
@@ -47,22 +47,15 @@ function M.config()
 			end,
 		},
 		window = {
-			documentation = cmp.config.disable
+			-- documentation = cmp.config.disable
 			-- completion = {
 			-- 	-- winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
 			-- 	col_offset = -3,
 			-- 	side_padding = 0,
 			-- },
-			-- documentation = cmp.config.window.bordered(
-			-- 	'╭',
-			-- 	'─',
-			-- 	'╮',
-			-- 	'│',
-			-- 	'╯',
-			-- 	'─',
-			-- 	'╰',
-			-- 	'│'
-			-- ),
+
+			--stylua: ignore
+			-- documentation = cmp.config.window.bordered( '╭', '─', '╮', '│', '╯', '─', '╰', '│'),
 		},
 		formatting = {
 			fields = { 'kind', 'abbr', 'menu' },
