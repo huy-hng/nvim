fd518ecffbaab04d723a1db03e33a0fd79e61692
Huy <huy@hoang.dev>
2023-03-18 22:37:12 +0100

    reload plugin on save

--- lua/plugin_management/lazy.lua
+++ lua/plugin_management/lazy.lua
@@ -10,3 +10,31 @@ require('lazy').setup({
 	{ import = 'plugins.lsp' },
 	{ import = 'plugins.ui' },
 }, require('plugin_management.config'))
+
+local function path_to_require(path)
+	path = path or vim.fn.expand('%')
+	if not path then return end
+
+	local relative = string.match(path, 'lua/(.*).lua$')
+	return string.gsub(relative, '/', '.')
+end
+
+local function reload_config(data)
+	-- P(data)
+	local path = npcall(path_to_require)
+	if not path then return end
+
+	local module = R(path)
+	-- P(module)
+	if not module or type(module) ~= 'table' or not module.config then return end
+
+	module.config()
+end
+
+-- reload plugin config on save
+Augroup('Alpha', {
+	Autocmd('User', 'VeryLazy', function(data)
+		NestedAutocmd(data, 'User', 'LazyReload', reload_config)
+		return true
+	end),
+})
