ad73b120bb41213c25656fa33c05784bc1b4cce7
Huy <huy@hoang.dev>
2023-03-19 23:46:41 +0100

    cleanup buffer keymaps for change to own buffer manager

--- lua/keymaps/buffers.lua
+++ lua/keymaps/buffers.lua
@@ -2,51 +2,6 @@
 Nmap('<leader>ba', { nvim.exec, '%bd | e# | bd#' }, 'Close all, except current')
 Nmap('<leader>bf', { nvim.exec, 'Telescope buffers' }, 'Fuzzy find')
 
-----------------------------------------
---          -> BufferLine <-
-----------------------------------------
-local loaded, bufferline = pcall(require, 'bufferline')
-if not loaded then
-	vim.notify('Bufferline not loaded for keymaps')
-	return
-end
-
-Nmap('<leader>bh', { bufferline.move, -1 }, 'Move Right')
-Nmap('<leader>bl', { bufferline.move, 1 }, 'Move Right')
-Nmap('<leader>bd', bufferline.close_buffer_with_pick, 'Pick')
-
-Nmap('<leader>bcd', { vim.fn.chdir, Util.wrap(vim.fn.expand, '%:p:h') }, 'Change dir to current buffer')
--- nmap('<leader>bd', bufferline.close_with_pick, 'Pick')
-
-Nmap('H', { bufferline.cycle, -1 }, 'Prev Buffer')
-Nmap('L', { bufferline.cycle, 1 }, 'Next Buffer')
-
-local absolute = true
-local jump_to_buffer = function(target_buffer)
-	local num_visible_buffers
-
-	bufferline.exec(1, function(buf, visible_buffers) num_visible_buffers = #visible_buffers end)
-
-	if target_buffer > num_visible_buffers then bufferline.group_action('Vimwiki', 'toggle') end
-
-	bufferline.go_to_buffer(target_buffer, absolute)
-end
-
-for i in ipairs(vim.fn.range(1, 9)) do
-	local keymap = string.format('<leader>%s', i)
-	Nmap(keymap, { jump_to_buffer, i }, 'Go to BufferLine')
-end
-
-Nmap('<leader>0', { bufferline.go_to_buffer, -1, absolute }, '')
--- bufferline.group_action('Vimwiki', function(buf)
--- 	P(buf)
--- end)
-
--- bufferline.exec(1, function(buf, visible_buffers)
--- 	P(buf)
--- 	-- P(visible_buffers)
--- end)
-
 function Group_bufferline_tabs()
 	local windows = vim.fn.getwininfo()
 	for _, win in ipairs(windows) do
@@ -54,4 +9,49 @@ function Group_bufferline_tabs()
 		local buffer = vim.fn.getbufinfo(win.bufnr)[1]
 		print(buffer.name)
 	end
+end
+
+local function get_buffers()
+	return vim.tbl_filter(
+		function(bufnr) return vim.api.nvim_buf_get_option(bufnr, 'buflisted') end,
+		vim.api.nvim_list_bufs()
+	)
+end
+
+local function buffer_cycle(val)
+	-- local buffers = get_buffers()
+	local buffers = require('plugins.ui.heirline.buffer_manager.ui').get_ordered_bufids()
+
+	local cur_buf = vim.api.nvim_get_current_buf()
+	local index = table.index(buffers, cur_buf)
+
+	local target = index + val
+
+	if target > #buffers then
+		target = 1
+	elseif target < 1 then
+		target = #buffers
+	end
+
+	vim.api.nvim_win_set_buf(0, buffers[target])
+end
+
+local function buffer_switch(index)
+	local buffers = get_buffers()
+	if index > #buffers or index == -1 then index = #buffers end
+	vim.api.nvim_win_set_buf(0, buffers[index])
+end
+
+-- Nmap('H', { buffer_cycle, -1 }, 'Prev Buffer')
+-- Nmap('L', { buffer_cycle, 1 }, 'Next Buffer')
+
+local buf_manager = require('plugins.ui.heirline.buffer_manager.ui')
+
+Nmap('H', buf_manager.nav_prev, 'Prev Buffer')
+Nmap('L', buf_manager.nav_next, 'Next Buffer')
+
+Nmap('<leader>0', { buffer_switch, -1 }, '')
+for i in ipairs(vim.fn.range(1, 9)) do
+	local keymap = string.format('<leader>%s', i)
+	Nmap(keymap, { buffer_switch, i }, 'Go to Buffer')
 end
