081c19ff8e29c4e50cd9b88b00bc48b561a486d0
Huy <huy@hoang.dev>
2023-07-25 19:32:08 +0200

    open alpha in new tab

--- lua/plugins/ui/alpha/init.lua
+++ lua/plugins/ui/alpha/init.lua
@@ -14,6 +14,19 @@ Augroup('Alpha', {
 	end),
 })
 
+local function open_alpha_in_tab()
+	local alpha = require('alpha')
+	vim.cmd.tabnew()
+	alpha.start(false)
+
+	Augroup('AlphaTab', {
+		Autocmd('BufLeave', nil, function()
+			nvim.schedule(pcall, vim.cmd.tabclose)
+			return true
+		end, { buffer = 0 }),
+	})
+end
+
 function M.config()
 	local utils = require('plugins.ui.alpha.utils')
 	if not utils.should_show() and not vim.g.has_neovide then return end
@@ -21,7 +34,7 @@ function M.config()
 	utils.change_native_keymap_fn()
 
 	local alpha = require('alpha')
-	Map.n('<leader>S', { alpha.start, false }, 'Open Start Screen')
+	Map.n('<leader>S', open_alpha_in_tab, 'Open Start Screen')
 
 	local layout = require('plugins.ui.alpha.layout')
 	alpha.setup(layout)
