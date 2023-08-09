88d273ed4a519e34ef7da2c68d3f2d574cea7c50
Huy <huy@hoang.dev>
2023-08-09 16:28:47 +0200

    add proper zen mode with transparent background

--- lua/plugins/ui/zen.lua
+++ lua/plugins/ui/zen.lua
@@ -6,27 +6,104 @@ local M = {
 	event = 'VeryLazy',
 }
 
+local statuscolumn = require('core.statuscolumn.statuscolumn')
+
+local zenmode_active = false
+local parent_tab = 1
+local zen_tab
+local default_tabline = "%{%v:lua.require'heirline'.eval_tabline()%}"
+local prev_tabline = default_tabline
+
+local function hide_ui()
+	prev_tabline = vim.o.tabline
+	vim.o.tabline = ' '
+	vim.schedule(function()
+		vim.o.laststatus = 0 -- global statusline
+		if nrequire('lualine') then --
+			require('lualine').hide {
+				place = { 'statusline', 'tabline', 'winbar' }, -- The segment this change applies to.
+				unhide = false, -- whether to re-enable lualine again/
+			}
+		end
+	end)
+end
+
+local function show_ui()
+	vim.o.tabline = prev_tabline ~= ' ' and prev_tabline or default_tabline
+	if nrequire('lualine') then --
+		require('lualine').hide { unhide = true }
+	end
+end
+
+local line_numbers = require('modules.line_numbers')
+local tab_control = require('modules.tab_control')
+
+local function new_tab() end
+
+local function activate()
+	local view = require('zen-mode.view')
+
+	zenmode_active = true
+	-- local tab_id, cursor_pos = tab_control.open_current_file_in_new_tab()
+	local parent_win = vim.api.nvim_get_current_win()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+
+	parent_tab = vim.api.nvim_get_current_tabpage()
+	vim.cmd.tabedit('#' .. vim.api.nvim_get_current_buf())
+	zen_tab = vim.api.nvim_get_current_tabpage()
+
+	local scratch_buf = vim.api.nvim_create_buf(false, true)
+	local tab_win = vim.api.nvim_get_current_win()
+
+	hide_ui()
+
+	require('zen-mode').open()
+	vim.api.nvim_win_set_buf(tab_win, scratch_buf)
+	view.parent = parent_win
+
+	vim.api.nvim_win_set_cursor(0, cursor)
+	nvim.feedkeys('zz')
+	nvim.defer(0, function()
+		line_numbers.delete_autocmds()
+		vim.wo.relativenumber = false
+		vim.wo.winbar = ''
+		statuscolumn.remove_statuscolumn(tab_win)
+	end)
+end
+
+local function deactivate()
+	zenmode_active = false
+
+	vim.o.laststatus = 3 -- global statusline
+	line_numbers.create_autocmds()
+	show_ui()
+
+	vim.schedule(function()
+		-- nvim.feedkeys('``')
+		-- nvim.feedkeys('zz')
+		vim.api.nvim_set_current_tabpage(parent_tab)
+		local i = table.index(vim.api.nvim_list_tabpages(), zen_tab)
+		if i then vim.cmd(i .. 'tabclose') end
+
+		vim.cmd.TwilightDisable()
+	end)
+end
+
 function M.config()
+	Map.n('<leader>Z', vim.cmd.Twilight, 'Twilight')
+	Map.n('<leader>z', function()
+		-- TODO: use view.is_open()
+		if zenmode_active then --
+			return deactivate()
+		end
+		activate()
+	end, 'Zen Mode')
+
 	require('zen-mode').setup {
 		window = {
 			backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-			-- height and width can be:
-			-- * an absolute number of cells when > 1
-			-- * a percentage of the width / height of the editor when <= 1
-			-- * a function that returns the width or the height
-			width = 100, -- width of the Zen window
-			height = 0.9, -- height of the Zen window
-			-- by default, no options are changed for the Zen window
-			-- uncomment any of the options below, or add other vim.wo options you want to apply
-			options = {
-				signcolumn = 'no', -- disable signcolumn
-				number = false, -- disable number column
-				relativenumber = false, -- disable relative numbers
-				-- cursorline = false, -- disable cursorline
-				-- cursorcolumn = false, -- disable cursor column
-				foldcolumn = '0', -- disable fold column
-				-- list = false, -- disable whitespace characters
-			},
+			width = 105,
+			height = 1,
 		},
 		plugins = {
 			-- disable some global vim options (vim.o...)
@@ -50,14 +127,49 @@ function M.config()
 		},
 		-- callback where you can add custom code when the Zen window opens
 		on_open = function(win)
-			-- print(win)
-			-- P(vim.api.nvim_list_tabpages())
-			-- Exec('tabnew')
-			-- P(vim.api.nvim_tabpage_get_win(0))
-			-- Schedule(vim.api.nvim_set_current_win, win)
+			local bg_win = win - 1
+			vim.wo[win].winblend = 100
+
+			local conf = {
+				anchor = 'NW',
+				col = {
+					[false] = 54,
+					[true] = 3,
+				},
+				external = false,
+				focusable = true,
+				height = 60,
+				relative = 'editor',
+				row = {
+					[false] = 0,
+					[true] = 3,
+				},
+				width = 105,
+				zindex = 40,
+			}
+			vim.api.nvim_win_close(bg_win, false)
+			nvim.defer(0, function()
+				local column = statuscolumn.build {
+					Statuscolumn.right_align,
+					Statuscolumn.sparse_line_number,
+					Statuscolumn.border,
+					Statuscolumn.space,
+				}
+				statuscolumn.set_statuscolumn(win, column)
+
+				vim.api.nvim_clear_autocmds {
+					event = { 'VimResized', 'CursorHold', 'CursorMoved' },
+					group = 'Zen',
+					pattern = '*',
+				}
+				local win_conf = vim.api.nvim_win_get_config(win)
+				win_conf.col = win_conf.col[false] + 15
+				vim.api.nvim_win_set_config(win, win_conf)
+				-- vim.wo[win].winhl = 'NormalFloat:NoiceMini'
+			end)
 		end,
 		-- callback where you can add custom code when the Zen window closes
-		on_close = function(win) vim.cmd.TwilightDisable() end,
+		on_close = function(win) deactivate() end,
 	}
 
 	require('twilight').setup {
