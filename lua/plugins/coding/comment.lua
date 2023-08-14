2ba0161cfdf53bf7689bce3b9d71b987d46c421d
Huy <huy@hoang.dev>
2023-08-15 00:32:10 +0200

    refactoring

--- lua/plugins/coding/comment.lua
+++ lua/plugins/coding/comment.lua
@@ -5,7 +5,36 @@ local M = {
 
 function M.config()
 	local Comment = require('Comment')
+
+	Comment.setup {
+		padding = true,
+		sticky = false,
+		ignore = '^$',
+		toggler = {
+			line = '', -- gcc
+			block = '', -- gbc
+		},
+		opleader = {
+			line = '', -- gc
+			block = '', -- gb
+		},
+		extra = {
+			above = '', -- gcO
+			below = '', -- gco
+			eol = '', -- gcA
+		},
+		mappings = {
+			basic = false,
+			extra = false,
+		},
+		pre_hook = nil,
+		post_hook = nil,
+	}
+
+	local api = require('Comment.api')
 	local ft = require('Comment.ft')
+	-- ft.set('vimwiki', '- %s')
+
 
 	local comment_empty = {
 		padding = true,
@@ -13,123 +42,32 @@ function M.config()
 		ignore = nil,
 	}
 
-	local conf = {
-		padding = true,
-		sticky = true,
-		ignore = '^$',
-		toggler = {
-			line = '',
-			block = '',
-		},
-		opleader = {
-			line = '',
-			block = '',
-		},
-		extra = {
-			above = '',
-			below = '',
-			eol = '',
-		},
-		mappings = {
-			basic = false,
-			extra = false,
-		},
-		pre_hook = nil,
-		post_hook = nil,
-	}
+	local function toggle_current() api.toggle.linewise.current(nil, comment_empty) end
 
-	local extra_conf = {
-		toggler = {
-			line = 'gcc',
-			block = 'gbc',
-		},
-		opleader = {
-			line = 'gc',
-			block = 'gb',
-		},
-		extra = {
-			above = 'gcO',
-			below = 'gco',
-			eol = 'gcA',
-		},
-		mappings = {
-			basic = false,
-			extra = false,
-		},
-		pre_hook = nil,
-		post_hook = nil,
-	}
-
-
-	Comment.setup(conf)
-	ft.set('vimwiki', '- %s')
-
-	local api = require('Comment.api')
-
-	-- map({ 'n', 'i' }, '<C-/>', api.locked('toggle.linewise.current'), 'Toggle Comment')
-
-	local ignore_empty = {
-		padding = true,
-		ignore = '^$',
-	}
-
-	local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
-
-	local toggle_comment = function()
-		-- api.toggle.blockwise('v')
-		-- api.toggle.linewise('V'j
-
-		local mode = vim.fn.mode()
-		if mode == 'n' or mode == 'i' then
-			api.toggle.linewise.current(nil, comment_empty)
-			return
-		end
-
-		vim.api.nvim_feedkeys(esc, 'nx', false)
-		if mode == 'v' then
-			-- api.locked('toggle.blockwise')(vim.fn.visualmode())
-			api.locked('toggle.linewise')(vim.fn.visualmode())
-		elseif mode == 'V' then
-			api.locked('toggle.linewise')(vim.fn.visualmode())
-			-- vim.api.nvim_command("lua require('Comment.api').toggle.linewise('V')")
-			vim.api.nvim_feedkeys('gv', 'nt', false)
-		elseif mode == '' then
-			-- TODO: doesnt work
-			-- api.toggle.linewise(vim.fn.visualmode(), ignore_empty)
-			api.locked('toggle.blockwise')(vim.fn.visualmode())
-		end
+	local function locked(mode, blockwise)
+		local cmd = 'toggle.' .. (blockwise and 'blockwise' or 'linewise')
+		return function() api.locked(cmd)(mode) end
 	end
 
-	-- neovide bindings
-	-- if vim.g.neovide then
-	local commap = Map.create({ 'n', 'i', 'v' }, '', '[Comment]')
+	local mode_behavior = {
+		i = toggle_current,
+		n = toggle_current,
+		v = locked('v'),
+		V = locked('V'),
+		[''] = locked('', true),
+	}
+	local toggle_comment = function()
+		local mode = vim.fn.mode()
+		nvim.feedkeys('<esc>', 'nx')
+		mode_behavior[mode]()
+	end
 
-	Map.nv(Keys.ctrl.comment, vim.schedule_wrap(toggle_comment), 'Toggle')
-
-	-- commap = Map.create({ 'n', 'i' }, '', '[Comment]')
-	-- map({ 'n', 'i' }, '<C-/>', fn(api.toggle.linewise.current, nil, comment_empty), 'Toggle Comment')
-	-- commap('<A-/>', api.insert.linewise.eol, 'Comment at end of line')
-	-- map({ 'n', 'i' }, '<A-/>a', api.insert.linewise.eol, 'Comment at end of line')
-
-	-- map({ 'n', 'i' }, '<A-/>o', api.insert.linewise.below, 'Comment next line')
-	-- map({ 'n', 'i' }, '<A-/>O', api.insert.linewise.above, 'Comment prev line')
-	-- else
-	-- 	map({ 'n', 'i', 'v' }, '<C-_>', toggle_comment, 'Toggle Comment')
-	-- 	map({ 'n', 'i' }, '<A-_>', api.insert.linewise.eol, 'Comment at end of line')
-	-- map(
-	-- 	{ 'n', 'i' },
-	-- 	'<C-_>',
-	-- 	fn(api.toggle.linewise.current, nil, comment_empty),
-	-- 	'Toggle Comment'
-	-- )
-	-- 	-- terminal bindings
-	-- 	map({ 'n', 'i' }, '<C-_>', fn(api.toggle.linewise.current, nil, comment_empty), 'Toggle Comment')
-	-- 	map({ 'n', 'i' }, '<A-_>a', api.insert.linewise.eol, 'Comment at end of line')
-	-- 	map({ 'n', 'i' }, '<A-_>', api.insert.linewise.eol, 'Comment at end of line')
-	-- 	map({ 'n', 'i' }, '<A-_>o', api.insert.linewise.below, 'Comment next line')
-	-- 	map({ 'n', 'i' }, '<A-_>O', api.insert.linewise.above, 'Comment prev line')
-	-- 	vmap('<C-_>', toggle_visual, 'Toggle Comment')
-	-- end
+	Map.map(
+		{ 'n', 'v', 'i' },
+		Keys.ctrl.comment,
+		vim.schedule_wrap(toggle_comment),
+		'Toggle Comment'
+	)
 end
 
 return M
