e47ebc25b509a5f73a110e82300c19bee3729bc1
Huy <huy@hoang.dev>
2023-08-15 06:21:41 +0200

    cleanup

---
+++ lua/modules/detect_indentation/init.lua
@@ -0,0 +1,2 @@
+require('modules.detect_indentation.detect_indentation')
+require('modules.detect_indentation.set_indentation')
--- lua/plugins/treesitter/node_action.lua
+++ lua/plugins/treesitter/node_action.lua
@@ -9,7 +9,6 @@ function M.config()
 	local na = require('ts-node-action')
 	na.setup()
 
-	-- Map.n(Keys.localleader.n, na.node_action, 'Trigger TS Node Action')
 	Map.n(Keys.ctrl.c, na.node_action, 'Trigger TS Node Action')
 
 end
