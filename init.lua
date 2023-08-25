66e009709f0d75dc0618c618f0be1f432b1d1775
Huy <huy@hoang.dev>
2023-08-25 17:33:38 +0200

    cleanup

--- init.lua
+++ init.lua
@@ -7,27 +7,33 @@ vim.g.neovide_fullscreen = true
 vim.g.has_neovide = vim.g.neovide
 vim.g.neovide = nil
 
+-- these shouldnt depend on anything except for vim itself
+require('config.keymaps')
+require('config.ui.colors')
+
 require('core.options')
 require('core.globals')
 require('core.types')
 require('core.colorscheme')
 require('core.statuscolumn')
 require('core.menus')
-require('config.native_keymaps')
+require('modules.detect_indentation')
+
 require('core.neovide')
 
 require('plugin_manager.lazy')
 SetColors('catppuccin-mocha')
 
--- Map('QQ', '<cmd>qa!<cr>')
+Map('QQ', '<cmd>qa<cr>')
 Augroup('LazyLoad', {
 	Autocmd('User', 'VeryLazy', function()
-		Map.n('<C-S-P>', require('lazy').home)
+		Map.n(Keys.plugin_manager, require('lazy').home)
 		local require_dir = require('modules.require_dir')
 		require_dir('lua/functions')
 		require_dir('lua/keymaps')
 		require('keymaps.layout_changer.colemak').set_keymap()
 		require('core.autocmd')
+		vim.cmd('let $PATH="/home/huy/.local/bin/:" .. $PATH')
 
 		return true
 	end),
