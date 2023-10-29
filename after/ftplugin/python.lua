9753fc2b1fb57a42877a83dc0a725be79ef23f50
Huy <huy@hoang.dev>
2023-10-28 17:16:54 -0700

    refactoring

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,23 +1,27 @@
-vim.opt_local.expandtab = false
+-- vim.opt_local.expandtab = false
+
+local noice = nrequire('noice')
 
 local function execute()
 	vim.cmd.write()
 	vim.cmd('!$PYTHONPATH %')
 end
-local cmd = execute
 
-local separator_length = 80
-
-local noice = nrequire('noice')
-if noice then --
-	cmd = function()
-		noice.redirect(function()
-			print('.' .. nvim.Repeat(' ', separator_length - 2) .. '.')
-			print(nvim.Repeat('-', separator_length))
-			print('.' .. nvim.Repeat(' ', separator_length - 2) .. '.')
-			execute()
-		end, { { view = false, filter = { event = 'msg_show' }, skip = true } })
-	end
+local function pretty_print_output()
+	local separator_length = 80
+	print('.' .. nvim.Repeat(' ', separator_length - 2) .. '.')
+	print(nvim.Repeat('-', separator_length))
+	print('.' .. nvim.Repeat(' ', separator_length - 2) .. '.')
+	execute()
 end
+
+local function redirect_output()
+	if not noice then return end
+	noice.redirect(pretty_print_output, {
+		{ view = false, filter = { event = 'msg_show' }, skip = true },
+	})
+end
+
+local cmd = noice and redirect_output or execute
 
 Map.n('<localleader>r', cmd, 'Run current Python file', { buffer = true })
