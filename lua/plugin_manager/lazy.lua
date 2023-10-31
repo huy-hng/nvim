71f1be616229f505c2762209b7d7d935aa121453
Huy <huy@hoang.dev>
2023-10-31 13:57:27 -0700

    change plugin specs

--- lua/plugin_manager/lazy.lua
+++ lua/plugin_manager/lazy.lua
@@ -1,14 +1,17 @@
 require('plugin_manager.ensure_lazy')
 local spec = {
-	{ import = 'plugins' },
-	{ import = 'plugins.coding' },
-	{ import = 'plugins.treesitter' },
+	{ import = 'plugins.basics' },
+	{ import = 'plugins.ui' },
+
+	{ import = 'plugins.editing' },
 	{ import = 'plugins.editor' },
 	{ import = 'plugins.git' },
+
 	{ import = 'plugins.lsp' },
-	{ import = 'plugins.ui' },
+	{ import = 'plugins.treesitter' },
 }
 
 require('lazy').setup(spec, require('plugin_manager.config'))
-
 require('plugin_manager.reload_on_save').start()
+
+Map.n(Keys.plugin_manager, require('lazy').home)
