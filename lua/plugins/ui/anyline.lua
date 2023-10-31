65b508eed0ed1b320e881681938f593baecf9213
Huy <huy@hoang.dev>
2023-10-31 13:59:21 -0700

    add command to enable/disable anyline and column line

--- lua/plugins/ui/anyline.lua
+++ lua/plugins/ui/anyline.lua
@@ -1,23 +1,27 @@
 local M = {
 	'huy-hng/anyline.nvim',
 	dev = true,
-	dependencies = { 'nvim-treesitter/nvim-treesitter' },
-	event = 'VeryLazy',
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter' },
+	-- event = 'VeryLazy',
+	event = 'BufReadPost',
 }
 
 function M.config()
 	require('anyline').setup {
-		-- highlight = 'Error',
 		-- animation = 'from_cursor',
 		-- animation = 'top_down',
 		lines_per_second = 100,
 		trail_length = 15,
 		fps = 30,
 	}
-	-- require('anyline.column_line').setup()
-	-- require('anyline.column_line').setup { buftype_whitelist = { 'dts' } }
-	require('anyline.column_line').setup {
-		buftype_blacklist = { 'dts' },
+
+	local column_line = require('anyline.column_line')
+
+	nvim.command('ColumnLineDisable', column_line.stop)
+	nvim.command('ColumnLineEnable', column_line.start)
+
+	column_line.setup {
+		buftype_blacklist = { 'dts', 'csv' },
 		-- wintype_whitelist = { '', 'popup' },
 		filetype_blacklist = {
 			'buffer_manager',
