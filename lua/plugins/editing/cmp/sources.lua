cbafb48e66201ad97c75b251d12d90cdd578c933
Huy <huy@hoang.dev>
2023-11-13 18:16:15 -0800

    add reference

--- lua/plugins/editing/cmp/sources.lua
+++ lua/plugins/editing/cmp/sources.lua
@@ -2,6 +2,67 @@ local cmp = require('cmp')
 
 local M = {}
 
+local ctx_example = {
+	aborted = false,
+	bufnr = 5,
+	cache = {
+		entries = {},
+		--   <metatable> = {
+		--     __index = {
+		--       clear = <function 1>,
+		--       ensure = <function 2>,
+		--       get = <function 3>,
+		--       key = <function 4>,
+		--       new = <function 5>,
+		--       set = <function 6>
+		--     }
+		--   }
+		-- },
+		cursor = {
+			character = 2,
+			col = 3,
+			line = 15,
+			row = 16,
+		},
+		cursor_after_line = '',
+		cursor_before_line = '\t\t',
+		cursor_line = '\t\t',
+		filetype = 'lua',
+		id = 25,
+		option = {},
+		prev_context = {
+			aborted = true,
+			bufnr = 5,
+			cursor = {
+				character = 2,
+				col = 3,
+				line = 15,
+				row = 16,
+			},
+			cursor_after_line = '',
+			cursor_before_line = '\t\t',
+			cursor_line = '\t\t',
+			filetype = 'lua',
+			id = 16,
+			option = {
+				reason = 'manual',
+			},
+			time = 65472154,
+		},
+		time = 65472384,
+		-- <metatable> = {
+		--   __index = {
+		--     abort = <function 7>,
+		--     changed = <function 8>,
+		--     clone = <function 9>,
+		--     empty = <function 10>,
+		--     get_offset = <function 11>,
+		--     get_reason = <function 12>,
+		--     new = <function 13>
+		--   }
+	},
+}
+
 M.sources = cmp.config.sources({
 	{ name = 'nvim_lsp_signature_help' }, -- shows function signature with current param emphasized
 	{ name = 'luasnip' }, -- put luasnip before lsp so that lsp snippets can be deduped
