354147814f8fcc0d0d9dd0632186fe87b8f74f1e
Huy <huy@hoang.dev>
2026-03-02 07:29:33 +0100

    change keymap

--- lua/keymaps/window_management.lua
+++ lua/keymaps/window_management.lua
@@ -103,7 +103,7 @@ local function center_two_windows(size)
 end
 
 Map.n('<leader>C', function() center_two_windows(120) end, 'Change current horizontal window size to 120')
-win_prefix.n('<C-c>', center_two_windows, 'In two win setup, center right win')
+win_prefix.n('<C-_>', center_two_windows, 'In two win setup, center right win')
 
 Augroup('WindowCenterer', {
 	-- Autocmd('WinEnter', function(data) vim.notify(tostring(vim.api.nvim_get_current_win())) end),
